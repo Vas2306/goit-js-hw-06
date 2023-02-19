a54d0215242a8c287d3141388a785443cf157e58
Vas2306 <vasilisa230679@gmail.com>
2023-02-19 16:48:44 +0100

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+
+const makeListIngredients = itemIngredients => {
+  return itemIngredients.map(item => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = item;
+    itemEl.classList.add('item');
+    
+    return itemEl;
+  });
+};
+
+const elements = makeListIngredients(ingredients);
+console.log(elements);
+
+listIngredients.append(...elements);
+  
