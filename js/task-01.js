0ba2daf1c22d2bf493b926a648b8bb528b34cdfa
Vas2306 <vasilisa230679@gmail.com>
2023-02-20 21:28:47 +0100

    format documents with prettier

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,43 @@
+console.log(
+  `Number of categories: ${
+    document.querySelector("#categories").children.length
+  }`
+);
 
-console.log(`Number of categories: ${document.querySelector('#categories').children.length}`);
+console.log(
+  `Category: ${document.querySelector(".item").firstElementChild.textContent}`
+);
 
-console.log(`Category: ${document.querySelector('.item').firstElementChild.textContent}`);
+console.log(
+  `Elements: ${
+    document.querySelector(".item").lastElementChild.children.length
+  }`
+);
 
-console.log(`Elements: ${document.querySelector('.item').lastElementChild.children.length}`);
+console.log(
+  `Category: ${
+    document.querySelector(".item").nextElementSibling.firstElementChild
+      .textContent
+  }`
+);
 
-console.log(`Category: ${document.querySelector('.item').nextElementSibling.firstElementChild.textContent}`);
+console.log(
+  `Elements: ${
+    document.querySelector(".item").nextElementSibling.lastElementChild.children
+      .length
+  }`
+);
 
-console.log(`Elements: ${document.querySelector('.item').nextElementSibling.lastElementChild.children.length}`);
+console.log(
+  `Category: ${
+    document.querySelector(".item").nextElementSibling.nextElementSibling
+      .firstElementChild.textContent
+  }`
+);
 
-console.log(`Category: ${document.querySelector('.item').nextElementSibling.nextElementSibling.firstElementChild.textContent}`);
-
-console.log(`Elements: ${document.querySelector('.item').nextElementSibling.nextElementSibling.lastElementChild.children.length}`);
+console.log(
+  `Elements: ${
+    document.querySelector(".item").nextElementSibling.nextElementSibling
+      .lastElementChild.children.length
+  }`
+);
--- js/task-02.js
+++ js/task-02.js
@@ -1,26 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
-const listIngredients = document.querySelector('#ingredients');
+const listIngredients = document.querySelector("#ingredients");
 
-const makeListIngredients = itemIngredients => {
-  return itemIngredients.map(item => {
-    const itemEl = document.createElement('li');
+const makeListIngredients = (itemIngredients) => {
+  return itemIngredients.map((item) => {
+    const itemEl = document.createElement("li");
     itemEl.textContent = item;
-    itemEl.classList.add('item');
-    
+    itemEl.classList.add("item");
+
     return itemEl;
   });
 };
 
 const elements = makeListIngredients(ingredients);
-console.log(elements);
+// console.log(elements);
 
 listIngredients.append(...elements);
-  
