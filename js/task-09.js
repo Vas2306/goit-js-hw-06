205905d4665227f086784afa545e99cd993f542e
Vas2306 <vasilisa230679@gmail.com>
2023-02-21 12:29:12 +0100

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+// const body = document.querySelector('body');
+
+// const button = document.querySelector('button.change-color');
+
+// button.addEventListener('click', getRandomHexColor);
